f304a5941a493fe6a000741a196f27b68c3155f6
Rewa Kawatra <rewa.kawatra@gmail.com>
2020-03-25 22:37:00 +0530

    Add new code

---
+++ LL-Merge-point.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the findMergeNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+      SinglyLinkedListNode temp1=head1;
+      SinglyLinkedListNode temp2=head2;
+      int res=0;
+      ArrayList<SinglyLinkedListNode> list = new ArrayList<>();
+      while(temp1!=null)
+      {
+        list.add(temp1); 
+        temp1=temp1.next;
+      }
+
+      while(temp2!=null)
+      {
+          if(list.contains(temp2))
+          {
+              res=temp2.data;
+              break;
+          }
+          temp2=temp2.next;
+      }
+      return res;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
