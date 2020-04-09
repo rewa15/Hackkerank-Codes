71e243535d5c886d783cf7c9e97354bc68180db7
Rewa Kawatra <rewa.kawatra@gmail.com>
2020-04-09 18:29:25 +0530

    Add new code

---
+++ The-Coin-ChangeDP.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'getWays' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. LONG_INTEGER_ARRAY c
+     */
+    
+
+    public static long getWays(int N, List<Long> c) 
+    {
+        int[] numCoins = new int[N+1];
+        numCoins[0] = 1;
+
+        for(int i = 0; i < c.size(); i++) 
+        {
+            long index = c.get(i);
+
+            for(int j = (int)index ; j < numCoins.length; j++) 
+            {
+                numCoins[j] += numCoins[j - (int)index];
+            }
+        }
+        return numCoins[N];
+    }
+    
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Long> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Long::parseLong)
+            .collect(toList());
+
+        // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+        long ways = Result.getWays(n, c);
+
+        bufferedWriter.write(String.valueOf(ways));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
