5ccce7b135627464dc1334e1c7d4f3d5e2ffd805
Rewa Kawatra <rewa.kawatra@gmail.com>
2020-04-21 00:19:03 +0530

    Update code

--- 0-1-Knapsack.java
+++ 0-1-Knapsack.java
@@ -1,30 +1,48 @@
-class Knapsack 
-{ 
-  
-     static int max(int a, int b) { return (a > b)? a : b; } 
-       
-     static int knapSack(int W, int wt[], int val[], int n) 
-     { 
-       
-      if (n == 0 || W == 0) 
-        return 0; 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+ {
+    // Code: Simple Recursion
+    public static int knapsack(int[] value, int[] weight, int w, int n)
+    {
+        // Choose the minimum possible valid input
+        // Base case
         
-      if (wt[n-1] > W) 
-       return knapSack(W, wt, val, n-1); 
-       
-      else return max( val[n-1] + knapSack(W-wt[n-1], wt, val, n-1), 
-                     knapSack(W, wt, val, n-1) 
-                      ); 
-      } 
-  
-    
-   // Driver program to test above function 
-   public static void main(String args[]) 
-   { 
-        int val[] = new int[]{60, 100, 120}; 
-        int wt[] = new int[]{10, 20, 30}; 
-    int  W = 50; 
-    int n = val.length; 
-    System.out.println(knapSack(W, wt, val, n)); 
-    } 
-} 
+        if(n==0 || w==0)
+         return 0;
+         
+        // Choice, diagram
+        if(weight[n-1] <= w)
+        {
+            return Math.max(value[n-1]+knapsack(value,weight,w-weight[n-1],n-1), knapsack(value,weight,w,n-1));
+        }
+        
+        else // when weight of n-1th item is greater than w, cannot include
+        {
+            return knapsack(value,weight,w,n-1);
+        }
+    }
+	public static void main (String[] args)
+	 {
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    for(int i=0; i<t; i++)
+	    {
+	        int n = sc.nextInt();
+	        int w = sc.nextInt();
+	        int[] value = new int[n];
+	        int[] weight = new int[n];
+	        for(int j=0; j<n; j++)
+	        {
+	            value[j] = sc.nextInt();
+	        }
+	        
+	        for(int k=0; k<n; k++)
+	        {
+	            weight[k] = sc.nextInt();
+	        }
+	        System.out.println(knapsack(value, weight, w, n));
+	    }
+	 }
+}
