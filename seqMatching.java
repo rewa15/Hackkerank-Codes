198c005e02edbd5231e285df37706479e7178ba4
Rewa Kawatra <rewa.kawatra@gmail.com>
2020-05-04 17:49:17 +0530

    create new file

---
+++ seqMatching.java
@@ -0,0 +1,53 @@
+// Seq pattern matching
+// Ques: https://www.youtube.com/watch?v=QVntmksK2es&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=32
+
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    public static boolean seq(String s1, String s2, int m, int n, int[][] dp)
+    {
+        for(int i=0; i<=m; i++)
+        {
+            for(int j=0; j<=n; j++)
+            {
+                if(i==0 || j==0)
+                 dp[i][j]=0;
+            }
+        }
+        
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=1; j<=n; j++)
+            { 
+                if(s1.charAt(i-1) == s2.charAt(j-1))
+                   dp[i][j] = 1 + dp[i-1][j-1];
+                   
+                else
+                   dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        if(dp[m][n]==m)
+         return true;
+        else
+         return false;
+        
+    }
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1 = sc.next();
+		String s2 = sc.next();
+		int m = s1.length();
+		int n = s2.length();
+		int[][] dp = new int[m+1][n+1];
+		System.out.println(seq(s1,s2,m,n,dp));
+	}
+}
+
+
+
+
+
