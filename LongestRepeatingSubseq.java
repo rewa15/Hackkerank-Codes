adde72ad79c561e7f8d59ce1721b0d019087c6d6
Rewa Kawatra <rewa.kawatra@gmail.com>
2020-05-04 16:39:59 +0530

    Create new file

---
+++ LongestRepeatingSubseq.java
@@ -0,0 +1,49 @@
+// Longest repeating subsequence
+// Ques link: https://www.geeksforgeeks.org/longest-repeated-subsequence/
+
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    public static int LRS(String s1, String s2, int m, int n, int[][] dp)
+    {
+        for(int i=0; i<=m; i++)
+        {
+            for(int j=0; j<=n; j++)
+            {
+                if(i==0 || j==0)
+                 dp[i][j]=0;
+            }
+        }
+        
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=1; j<=n; j++)
+            { 
+                if(s1.charAt(i-1) == s2.charAt(j-1) && i!=j)
+                   dp[i][j] = 1 + dp[i-1][j-1];
+                   
+                else
+                   dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        return dp[m][n];
+        
+    }
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1 = sc.next();
+		String s2=s1;
+		int m = s1.length();
+		int n = s2.length();
+		int[][] dp = new int[m+1][n+1];
+		System.out.println(LRS(s1,s2,m,n,dp));
+	}
+}
+
+
+
+
